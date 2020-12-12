98207a600f8c45cf80d1e0ec33711af6b1d2453c
Thomas Weber <weber_t@mailbox.org>
2020-12-11 20:17:16 -0600

    Revert "Use state manager"
    This reverts commit ccfa0e12ca8d989c9c73b4dd0ef08ec1a7a578ed.

--- src/playground/packager.jsx
+++ src/playground/packager.jsx
@@ -4,7 +4,6 @@ import {compose} from 'redux';
 import AppStateHOC from '../lib/app-state-hoc.jsx';
 import TWFullScreenHOC from '../lib/tw-embed-fullscreen-hoc.jsx';
 import TWFullscreenResizerHOC from '../lib/tw-fullscreen-resizer-hoc.jsx';
-import TWStateManagerHOC from '../lib/tw-state-manager-hoc.jsx';
 import TWPackagerHOC from './tw-packager-hoc.jsx';
 import ErrorBoundaryHOC from '../lib/error-boundary-hoc.jsx';
 
@@ -14,14 +13,9 @@ const appTarget = document.createElement('div');
 document.body.appendChild(appTarget);
 document.body.classList.add('tw-loaded');
 
-try {
-    history.replaceState('', '', location.pathname);
-} catch (e) {}
-
 const WrappedGUI = compose(
     AppStateHOC,
     ErrorBoundaryHOC('Packager'),
-    TWStateManagerHOC,
     TWFullscreenResizerHOC,
     TWFullScreenHOC,
     TWPackagerHOC
@@ -29,5 +23,4 @@ const WrappedGUI = compose(
 
 ReactDOM.render(<WrappedGUI
     isEmbedded
-    routingStyle={"none"}
 />, appTarget);
