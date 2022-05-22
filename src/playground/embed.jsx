4125cc24a65b669b1dc872645fa0008264809ca8
Thomas Weber <muffin@muffin.ink>
2022-05-21 21:59:53 -0500

    www-refactor: Simplify embed page

--- src/playground/embed.jsx
+++ src/playground/embed.jsx
@@ -6,32 +6,15 @@ import {compose} from 'redux';
 import {setAppElement} from 'react-modal';
 import AppStateHOC from '../lib/app-state-hoc.jsx';
 import TWEmbedFullScreenHOC from '../lib/tw-embed-fullscreen-hoc.jsx';
+import HashParserHOC from '../lib/hash-parser-hoc.jsx';
 import runAddons from '../addons/entry';
 
 import GUI from './render-gui.jsx';
 import appTarget from './app-target';
 
-const getProjectId = () => {
-    // For compatibility reasons, we first look at the hash.
-    // eg. https://turbowarp.org/embed.html#1
-    const hashMatch = location.hash.match(/#(\d+)/);
-    if (hashMatch !== null) {
-        return hashMatch[1];
-    }
-    // Otherwise, we'll recreate what "wildcard" routing does.
-    // eg. https://turbowarp.org/1/embed
-    const pathMatch = location.pathname.match(/(\d+)\/embed/);
-    if (pathMatch !== null) {
-        return pathMatch[pathMatch.length - 1];
-    }
-    return '0';
-};
-
-const projectId = getProjectId();
 const urlParams = new URLSearchParams(location.search);
 
 let vm;
-
 const onVmInit = _vm => {
     vm = _vm;
 };
@@ -45,16 +28,15 @@ const onProjectLoaded = () => {
 
 const WrappedGUI = compose(
     AppStateHOC,
+    HashParserHOC,
     TWEmbedFullScreenHOC
 )(GUI);
 
 setAppElement(appTarget);
 ReactDOM.render(<WrappedGUI
     isEmbedded
-    projectId={projectId}
     onVmInit={onVmInit}
     onProjectLoaded={onProjectLoaded}
-    routingStyle="none"
 />, appTarget);
 
 if (urlParams.has('addons')) {
