6bd242a9a9d77e46b543eec373384c51770e47b2
Thomas Weber <muffin@muffin.ink>
2022-05-21 22:35:27 -0500

    www-refactor: Import scratch-base.css in render-gui.jsx

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GUI from '../containers/gui.jsx';
+import './scratch-base.css';
 
 const searchParams = new URLSearchParams(location.search);
 const cloudHost = searchParams.get('cloud_host') || 'wss://clouddata.turbowarp.org';
