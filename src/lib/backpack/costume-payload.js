b360f302c2cc8696fb89c165ffc1f1c58329516d
GarboMuffin <muffin@muffin.ink>
2022-07-30 00:59:17 -0500

    Remove redundant calls to replace
    getExportedCostumeBase64 returns a base64 string, not a data URI

--- src/lib/backpack/costume-payload.js
+++ src/lib/backpack/costume-payload.js
@@ -17,16 +17,15 @@ const costumePayload = (costume, vm) => {
     switch (assetDataFormat) {
     case 'svg':
         payload.mime = 'image/svg+xml';
-        payload.body = assetDataUrl.replace('data:image/svg+xml;base64,', '');
+        payload.body = assetDataUrl;
         break;
     case 'png':
         payload.mime = 'image/png';
-        payload.body = assetDataUrl.replace('data:image/png;base64,', '');
+        payload.body = assetDataUrl;
         break;
     case 'jpg':
         payload.mime = 'image/jpeg';
-        // The data URL from scratch-storage will always claim to be a PNG even when it's not.
-        payload.body = assetDataUrl.replace('data:image/png;base64,', '');
+        payload.body = assetDataUrl;
         break;
     default:
         alert(`Cannot serialize for format: ${assetDataFormat}`); // eslint-disable-line
