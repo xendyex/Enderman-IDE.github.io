885d8244cf4bd638bb346de7ab93a186d0cfa60d
jwklong <jwklong@users.noreply.github.com>
2022-11-10 17:55:55 +0000

    deploy: f7b043dc1faccb93dfc40c4bc33500cde206301e

--- js/addon-default-entry.js
+++ js/addon-default-entry.js
@@ -1076,9 +1076,18 @@ __webpack_require__.r(__webpack_exports__);
       }, noopSwitch];
       blockSwitches["operator_and"] = [noopSwitch, {
         opcode: "operator_or"
+      }, {
+        opcode: "operator_xor"
       }];
       blockSwitches["operator_or"] = [{
         opcode: "operator_and"
+      }, noopSwitch, {
+        opcode: "operator_xor"
+      }];
+      blockSwitches["operator_xor"] = [{
+        opcode: "operator_and"
+      }, {
+        opcode: "operator_or"
       }, noopSwitch];
     }
 
